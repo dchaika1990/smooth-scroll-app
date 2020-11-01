ad8c8859804f59e1ae7e07c554d5abfccc8b63f4
dchaika1990 <dchaika@inbox.ru>
2020-11-01 23:15:29 +0200

    start app

---
+++ app.js
@@ -0,0 +1,29 @@
+function smoothScroll(target, duration) {
+    let $target = document.querySelector(target);
+    let targetPosition = $target.getBoundingClientRect().top;
+    let startPosition = window.pageYOffset;
+    let distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime){
+        if (!startTime) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+        let run = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function easeInOutQuad(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+let section1 = document.querySelector('.section1');
+section1.addEventListener('click', function (){
+    smoothScroll('.section2', 2000);
+})
